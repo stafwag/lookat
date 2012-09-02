587aff570c554d1343a03b14fdfe0f7d052e299a
Staf Wagemakers <staf@wagemakers.be>
2012-09-02 14:47:23 +0200

    prepare 1.4.2rc2 release

--- ned.h
+++ ned.h
@@ -26,7 +26,7 @@ const int sel_om                ='o';
 const int sel_hm		='h';
 char  *txt_help[] = {
 "---------------------------------------------------------------------------",
-"bekijk 1.4.2rc1                             (GPL) 1997-2011 Belgie  Belgium",
+"bekijk 1.4.2rc2                             (GPL) 1997-2011 Belgie  Belgium",
 "                                                            Staf Wagemakers",
 "                                                  email: staf@wagemakers.be",
 " ",
@@ -129,7 +129,7 @@ char *txt_attributen[] = {"normaal","dik","onderlijnd","reverse",
 char txt_ondersheid_g[]	="[ ] grootte & kleine letters  ";
 char txt_search_back[]  ="[ ] Achteruit zoeken";
 char *txt_over[]	={
-"Bekijk versie 1.4.2rc1             GNU Public License",
+"Bekijk versie 1.4.2rc2             GNU Public License",
 "Staf Wagemakers                    (1997-2007) Belgie","\n",
 "email:                             staf@wagemakers.be",
 "homepagina:                  http://www.wagemakers.be","\n",NULL};

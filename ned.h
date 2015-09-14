a50a188f34e58b168c7c08dd9d53aec762d443b3
Staf Wagemakers <staf@wagemakers.be>
2015-09-14 12:01:46 +0200

    end (c) date corrected

--- ned.h
+++ ned.h
@@ -26,7 +26,7 @@ const int sel_om                ='o';
 const int sel_hm		='h';
 char  *txt_help[] = {
 "---------------------------------------------------------------------------",
-"bekijk 1.4.4rc1                             (GPL) 1997-2013 Belgie  Belgium",
+"bekijk 1.4.4rc1                             (GPL) 1997-2015 Belgie  Belgium",
 "                                                            Staf Wagemakers",
 "                                                  email: staf@wagemakers.be",
 " ",
@@ -130,7 +130,7 @@ char txt_ondersheid_g[]	="[ ] grootte & kleine letters  ";
 char txt_search_back[]  ="[ ] Achteruit zoeken";
 char *txt_over[]	={
 "Bekijk versie 1.4.4rc1             GNU Public License",
-"Staf Wagemakers                    (1997-2013) Belgie","\n",
+"Staf Wagemakers                    (1997-2015) Belgie","\n",
 "email:                             staf@wagemakers.be",
 "homepagina:                  http://www.wagemakers.be","\n",NULL};
 char txt_bedank[]       ="Bedankt voor het gebruik van dit programma!";

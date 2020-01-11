43d7f8dd8e8683fc0600d682d4b525f33c8764c1
Staf Wagemakers <staf@wagemakers.be>
2020-01-11 11:05:37 +0100

    code cleanup

--- view.c
+++ view.c
@@ -26,14 +26,23 @@ VIEW_PAR *view_par (VIEW_PAR *par)
 if (par!=NULL) p=par;
   return (p);
 }
-unsigned view_strlen(char *c)
+
+unsigned view_strlen(char *str)
 {
   unsigned u=0;
+  unsigned charLength=strlen(str);
+  unsigned pointer=0; 
+  char *c;
+  c=str;
+
 
   while (*c) {
 
       u=u+1;
-      c=c+view_charstr_len(c);
+      pointer=pointer+view_charstr_len(c);
+
+      if(pointer>charLength) return u;
+      c=str+pointer;
 
   }
 
@@ -77,160 +86,214 @@ void view_free()
   view_free_file();
   free_string_array(*p->view_exec);
 }
-int get_strlen (char *s)
-{
-int xmax=0;
-while (*s) {
-  if (*s!=8) ++xmax;
-    else --xmax;
-  s++;
-  }
-return xmax;
-}
-/* ---------------------------------------------------- */ 
-/* Inladen van te bekijken bestand ...            */
-/*                    */
-/* aanroep  :           */
-/*              */
-/* char filename[] = te lezen bestand           */
-/* char **file      = ptr naar geh. pl.           */
-/*              */
-/* antwoord :           */
-/* 0 = ok                       */
-/* 1 = nok                                        */
-/*              */
-/* ---------------------------------------------------- */ 
-int view_load ()
-{
-FILE *fp=NULL;
-char *cp,***ccc;
-char *s;
-int i;
-size_t ii;
-/* ----------------- vari voor het starten van gzip ---------------------- */
-int  is_gz=0;
-char *gzip=NULL;
-FILE *popen(),*gz_fp; 
-/* ----------------------------------------------------------------------- */
-ccc=p->view_exec;
-if(p->filename!=NULL) {
-   while(*ccc) {
-     if(strlen(p->filename)>strlen(ccc[0][0])) { 
-       s=p->filename+strlen(p->filename)-strlen(ccc[0][0]);
-     }
-     else {
-       s=p->filename;
-     }
-     if(!strcmp(s,ccc[0][0])){  
-         gzip=(char *)xmalloc(strlen(ccc[0][1])+strlen(p->filename)+2);
-         strcpy(gzip,ccc[0][1]);
-         strcat(gzip," ");
-         strcat(gzip,p->filename);
-         break;
-     }
-     ++ccc;
-   }
-}
 
-if(gzip) {
-  int sys_err;
-  
-  if (p->cmd==1) {
-    if (win_freopen("/dev/null","w",stderr,p->m_ok,p->win)) return(1);
-  }
-  if(freopen("/dev/null","w",stdout) ==NULL) {
-    if(win_freopen("/dev/tty","w",stdout,p->m_ok,p->win)) return(1);
-    char *txt[]={p->txt_f_freopen,NULL};
-    open_okwin(6,40,p->m_ok,txt,p->win);
-    return(1);
-  }
-  sys_err=system(gzip);
-  if (sys_err==0) {
-     gz_fp=popen(gzip,"r");
-     if (gz_fp==NULL) pclose(fp);
-        else  is_gz=1;
-     } 
-      
-  if(win_freopen("/dev/tty","w",stdout,p->m_ok,p->win)) return(1);
-  if (p->cmd==1) {
-    if(win_freopen("/dev/tty","w",stderr,p->m_ok,p->win)) return(1);
-    return(1);
-  }
-  xfree(gzip);
-}
-if (p->cmd<2) {
-  if ((fp=fopen(p->filename,"r"))==NULL) {
-    if (p->cmd==1) {
-      fprintf(stderr,"%s %s %s",p->txt_f_open1,p->filename,p->txt_f_open2); 
-      if (COLS) endwin();
-      exit(1);
+/*
+ * Inladen van te bekijken bestand ...
+ *
+ * aanroep  :
+ *
+ * char filename[] = te lezen bestand
+ * char **file      = ptr naar geh. pl.
+ *
+ * antwoord :
+ * 0 = ok
+ * 1 = nok
+ *
+ */
+int view_load () {
+
+  FILE *fp=NULL;
+  char *cp,***ccc;
+  char *s;
+  int i;
+  size_t ii;
+  /* ----------------- vari voor het starten van gzip ---------------------- */
+  int  is_gz=0;
+  char *gzip=NULL;
+  FILE *popen(),*gz_fp; 
+  /* ----------------------------------------------------------------------- */
+  ccc=p->view_exec;
+
+  if (p->filename!=NULL) {
+
+    while(*ccc) {
+
+      if (strlen(p->filename)>strlen(ccc[0][0])) { 
+        s=p->filename+strlen(p->filename)-strlen(ccc[0][0]);
+      } else {
+        s=p->filename;
       }
-    else {
-      char *txt[]={p->txt_f_load,NULL};
+
+      if (!strcmp(s,ccc[0][0])) {  
+
+        gzip=(char *)xmalloc(strlen(ccc[0][1])+strlen(p->filename)+2);
+        strcpy(gzip,ccc[0][1]);
+        strcat(gzip," ");
+        strcat(gzip,p->filename);
+        break;
+
+      }
+
+      ++ccc;
+
+    }
+
+  }
+
+  if (gzip) {
+
+    int sys_err;
+  
+    if (p->cmd==1) {
+      if (win_freopen("/dev/null","w",stderr,p->m_ok,p->win)) return(1);
+    }
+
+    if (freopen("/dev/null","w",stdout) ==NULL) {
+
+      if (win_freopen("/dev/tty","w",stdout,p->m_ok,p->win)) return(1);
+      char *txt[]={p->txt_f_freopen,NULL};
       open_okwin(6,40,p->m_ok,txt,p->win);
       return(1);
+
+    }
+
+    sys_err=system(gzip);
+
+    if (sys_err==0) {
+
+      gz_fp=popen(gzip,"r");
+      if (gz_fp==NULL) pclose(fp);
+      else  is_gz=1;
+
+    } 
+      
+    if (win_freopen("/dev/tty","w",stdout,p->m_ok,p->win)) return(1);
+
+    if (p->cmd==1) {
+
+      if(win_freopen("/dev/tty","w",stderr,p->m_ok,p->win)) return(1);
+      return(1);
+
+    }
+
+    xfree(gzip);
+  }
+
+  if (p->cmd<2) {
+
+    if ((fp=fopen(p->filename,"r"))==NULL) {
+
+      if (p->cmd==1) {
+        fprintf(stderr,"%s %s %s",p->txt_f_open1,p->filename,p->txt_f_open2); 
+        if (COLS) endwin();
+        exit(1);
+      } else {
+
+        char *txt[]={p->txt_f_load,NULL};
+        open_okwin(6,40,p->m_ok,txt,p->win);
+        return(1);
+
       };
-   };
-};
-if (p->load) view_free_file();
-p->y=p->sy=0;p->sx=p->x=0;p->x_max=0;p->y_max=0;
-if (p->cmd<2) {
-  if (fseek(fp,0,SEEK_END)) {
-  char *txt[]={p->txt_f_fseek,NULL};
-  open_okwin(6,40,p->m_ok,txt,p->win);
-  return(1);
+
+    };
+
   };
-};
-if (p->cmd<2) {
-   p->size=ftell(fp);
-   fseek(fp,0,SEEK_SET); }
-  else p->size=0;
-if (is_gz) {fclose(fp);fp=gz_fp;};
-p->file=NULL;
-ii=0;
-do {
+
+  if (p->load) view_free_file();
+
+  p->y=p->sy=0;p->sx=p->x=0;p->x_max=0;p->y_max=0;
+
+  if (p->cmd<2) {
+
+    if (fseek(fp,0,SEEK_END)) {
+
+      char *txt[]={p->txt_f_fseek,NULL};
+      open_okwin(6,40,p->m_ok,txt,p->win);
+      return(1);
+
+    };
+
+  };
+
+  if (p->cmd<2) {
+
+    p->size=ftell(fp);
+    fseek(fp,0,SEEK_SET);
+
+  } else p->size=0;
+
+  if (is_gz) {
+
+    fclose(fp);fp=gz_fp;
+  
+  };
+
+  p->file=NULL;
+
+  ii=0;
+
+  do {
+
      int z;
      p->file=(char **) xrealloc(p->file,(p->y_max+2)*sizeof(char **));
+
      *(p->file+p->y_max)=NULL;
+
      if (p->cmd==2) p->x=getdelim(p->file+p->y_max,&ii,'\n',stdin);
        else p->x=getdelim(p->file+p->y_max,&ii,'\n',fp);
+
      if (p->x>0) {
 
         if (p->cmd==2) p->size+=p->x;
 
-  if (p->x>1) {
+        if (p->x>1) {
 
-  if (*(p->file[p->y_max]+p->x-2)==13) { 
-    *(p->file[p->y_max]+p->x-2)=10;
-    *(p->file[p->y_max]+p->x-1)=0;
-    };
-  }
+          if (*(p->file[p->y_max]+p->x-2)==13) { 
+
+            *(p->file[p->y_max]+p->x-2)=10;
+            *(p->file[p->y_max]+p->x-1)=0;
+
+          };
+
+        }
 
 
-  for (i=0;i<strlen(p->file[p->y_max]);i++) {
-     if (*(p->file[p->y_max]+i)==TAB) {
-       s=(char *) xmalloc(strlen(p->file[p->y_max])+(((i/8)+1)*8-i)+1);
-       strcpy(s,p->file[p->y_max]);
-       for (z=0;z<((i/8)+1)*8-i;z++) strcpy((s+i+z)," ");
-       strcpy((s+i+z),(p->file[p->y_max]+i+1));
-       cp=*(p->file+p->y_max);
-       cp=(char *)xrealloc(cp,strlen(s)+1);
-       *(p->file+p->y_max)=cp;
-       strcpy(p->file[p->y_max],s);
-       free(s);
-      } 
-   } 
-       if ((get_strlen(p->file[p->y_max])>p->x_max)&&(p->x!=-1)) p->x_max=get_strlen(p->file[p->y_max]); 
-       ++p->y_max;
-       }
+        for (i=0;i<strlen(p->file[p->y_max]);i++) {
+
+          if (*(p->file[p->y_max]+i)==TAB) {
+
+              s=(char *) xmalloc(strlen(p->file[p->y_max])+(((i/8)+1)*8-i)+1);
+              strcpy(s,p->file[p->y_max]);
+
+              for (z=0;z<((i/8)+1)*8-i;z++) strcpy((s+i+z)," ");
+              strcpy((s+i+z),(p->file[p->y_max]+i+1));
+              cp=*(p->file+p->y_max);
+              cp=(char *)xrealloc(cp,strlen(s)+1);
+              *(p->file+p->y_max)=cp;
+              strcpy(p->file[p->y_max],s);
+              xfree(s);
+
+          } 
+
+        } 
+
+        if ( (view_strlen(p->file[p->y_max])>p->x_max) && (p->x!=-1) ) p->x_max=view_strlen(p->file[p->y_max]); 
+        ++p->y_max;
+
+      }
+
    } while (p->x!=-1);
-if ((p->cmd<2)&&(!is_gz)) fclose(fp);
-if (is_gz) pclose(fp);
-p->load=1;
-if (p->x_max>p->cols) p->x_max-=p->cols; 
-  else p->x_max=0;
-p->x=p->y=0;
-return(0);
+
+  if ((p->cmd<2)&&(!is_gz)) fclose(fp);
+  if (is_gz) pclose(fp);
+  p->load=1;
+
+  if (p->x_max>p->cols) p->x_max-=p->cols; 
+    else p->x_max=0;
+
+  p->x=p->y=0;
+  return(0);
+
 }
 /* ----------------------------------------- */
 /* Print een kar. op het scherm, filter voor */
@@ -480,30 +543,40 @@ if (p->y>=2*(p->lines)) p->y-=2*(p->lines);
    else p->y=0; 
 view_refresh(); 
 }
-/* ---------------------------------------------------- */ 
-/* Scherm naar rechts ...       */
-/* ---------------------------------------------------- */ 
-void view_right()
-{
-if (!p->mode) {
-   if (p->x<p->x_max) {
+
+/*
+ * Scherm naar rechts ...
+ */
+void view_right() {
+
+  if (!p->mode) {
+
+    if (p->x<p->x_max) {
+
       ++p->x;
       p->y-=p->lines;
       view_refresh();
-      }
-   }
-   else {
-        if (p->x+p->sx<view_strlen(p->file[p->sy+p->y-p->lines])-1) {
+
+     }
+
+   } else {
+
+      if (p->x+p->sx<view_strlen(p->file[p->sy+p->y-p->lines])-1) {
+
         if (++p->sx>=p->cols) {
-        p->sx=p->cols-7;
-              p->y-=p->lines;
-              p->x+=7;
-              view_refresh();
+
+          p->sx=p->cols-7;
+          p->y-=p->lines;
+          p->x+=7;
+          view_refresh();
+
         }
-        else  view_set_cursor();
-  }
+        else view_set_cursor();
+      }
+
    }
 }
+
 /* ---------------------------------------------------- */ 
 /* Scherm naar links ...        */
 /* ---------------------------------------------------- */ 
